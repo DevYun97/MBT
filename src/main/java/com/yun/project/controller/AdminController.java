a9311823cc97415035198a4c1255bf05186182d9
DevYun97 <yundh812@gmail.com>
2022-07-03 11:06:08 +0900

    관리자 컨트롤러 코드정리

--- src/main/java/com/yun/project/controller/AdminController.java
+++ src/main/java/com/yun/project/controller/AdminController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.yun.project.dao.IboardDAO;
 import com.yun.project.dao.IqnaDAO;
 import com.yun.project.dao.IuserDAO;
 import com.yun.project.dto.Qna;
@@ -26,104 +27,90 @@ public class AdminController {
 	@Autowired
 	IqnaDAO qnaDao;
 	@Autowired
+	IboardDAO boardDao;
+	@Autowired
 	IuserDAO userDao;
 	@Autowired
 	UserService userService;
 	
+	// 관리자 페이지
 	@RequestMapping("admin")
 	public String admin(
 				@RequestParam Map <String, Object> map,
 				HttpSession session, Model model ){
-		
-				
-			ArrayList<User> memList = userDao.userList(map);
-			model.addAttribute("member", memList);
-			return "admin/admin";
+						
+		int boardCount = boardDao.boardCount(map);
+		model.addAttribute("bCount", boardCount);
+		return "admin/admin";
 	}
 	
+	// 관리자 QnA 페이지
 	@RequestMapping("adminQna")
 	public String adminQna(
 			@RequestParam Map <String, Object> map,
 			HttpSession session, Model model){
 		
-		/*
-		 * String user_rank; try { user_rank = (String)
-		 * session.getAttribute("user_rank"); } catch (NullPointerException e) {
-		 * user_rank = null; } if( user_rank == null) { return
-		 * "<script>location.href='../main';</script>"; } if( user_rank.equals("관리자") ||
-		 * user_rank.equals("매니저") ) {
-		 * 
-		 * ArrayList<Qna> getQnaList = qnaDao.getQnaList(map);
-		 * model.addAttribute("getQnaList", getQnaList); return
-		 * "<script>location.href='adminQna';</script>"; } return
-		 * "<script>location.href='../main';</script>";
-		 */
-			
-			ArrayList<Qna> getQnaList = qnaDao.getQnaList(map);
-			model.addAttribute("getQnaList", getQnaList);
-			return "admin/adminQna";
+		ArrayList<Qna> getQnaList = qnaDao.getQnaList(map);
+		model.addAttribute("getQnaList", getQnaList);
+		return "admin/adminQna";
 	}
 		
-	  @RequestMapping("qnaAjax")	  
-	  public String qnaAjax(	  
-			  @RequestParam Map <String, Object> map,
-			  HttpSession session, Model model ){	  
+	@RequestMapping("qnaAjax")	  
+	public String qnaAjax(	  
+			@RequestParam Map <String, Object> map,
+			HttpSession session, Model model ){	  
 		  
-		  	ArrayList<Qna> getQnaList = qnaDao.getQnaList(map);
-			model.addAttribute("getQnaList", getQnaList);
-			return "admin/qnaList";
+		ArrayList<Qna> getQnaList = qnaDao.getQnaList(map);
+		model.addAttribute("getQnaList", getQnaList);
+		return "admin/qnaList";
 			
-	  }
+	}
 	  
-	  //회원 리스트 불러오기
-	  @RequestMapping("member")
-		public String member(
-					@RequestParam Map <String, Object> map, Model model ){
-					
-				ArrayList<User> memList = userDao.userList(map);
-				model.addAttribute("member", memList);
-				return "admin/member";
-		}
+	//회원 리스트 불러오기
+	@RequestMapping("member")
+	public String member(@RequestParam Map <String, Object> map, Model model ){
+								
+		ArrayList<User> memList = userDao.userList(map);
+		model.addAttribute("member", memList);
+		return "admin/member";
+	}
 	  
-	  //memberList Ajax
-	  @RequestMapping("memAjax")	  
-	  public String memAjax(	  
-			  @RequestParam Map <String, Object> map,
-			  Model model ){	  
-		  	ArrayList<User> memList = userDao.userList(map);
-			model.addAttribute("member", memList);
-			return "admin/memList";
-			
-	  }
+	//memberList Ajax
+	@RequestMapping("memAjax")	  
+	public String memAjax(@RequestParam Map <String, Object> map, Model model ){	  
+			  	  			  
+		ArrayList<User> memList = userDao.userList(map);
+		model.addAttribute("member", memList);
+		return "admin/memList";			
+	}
 	  
-	  //회원정보 상세보기
-	  @RequestMapping("memberInfo")
-		public String memberInfo( int user_idx, Model model ){
-		  		User memInfo = userDao.getMemberInfo(user_idx);
-		  		model.addAttribute("mem", memInfo);
-				return "admin/memberInfo";
-		}
+	//회원정보 상세보기
+	@RequestMapping("memberInfo")
+	public String memberInfo( int user_idx, Model model ){
+		User memInfo = userDao.getMemberInfo(user_idx);
+		model.addAttribute("mem", memInfo);
+		return "admin/memberInfo";
+	}
 	  
-	  //관리자용 회원관리 (회원정보 복구)
-	  @RequestMapping("memUseUpdate")
-		@ResponseBody
-		public String quitAction(
+	//관리자용 회원관리 (회원정보 복구)
+	@RequestMapping("memUseUpdate")
+	@ResponseBody
+	public String quitAction(
 				@RequestParam ("user_idx") int user_idx,
 				@RequestParam ("useYN") String useYN,
-					HttpSession session, HttpServletRequest request ) {
-			String result = userService.quit(user_idx, useYN);
-			return result;
-		
-		}
+				HttpSession session, HttpServletRequest request ) {
+		String result = userService.quit(user_idx, useYN);
+		return result;		
+	}
 	  
-	  @RequestMapping("realQuit")
-		@ResponseBody
-		public String realQuit(
-				@RequestParam ("user_idx") int user_idx,
-					HttpSession session, HttpServletRequest request ) {
-			String result = userService.realQuit(user_idx);
-			return result;
-		
-		}
+	//회원정보 영구 삭제
+	@RequestMapping("realQuit")
+	@ResponseBody
+	public String realQuit(
+			@RequestParam ("user_idx") int user_idx,
+			HttpSession session, HttpServletRequest request ) {
+		String result = userService.realQuit(user_idx);
+		return result;		
+	}
 	  
 }
